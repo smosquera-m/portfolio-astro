dc44cb8eef1211498565448eb285cd803e65e57f
SermosmaDotLag <smosqueramarin@gmail.com>
2026-03-16 20:18:50 +0100

    Initial commit: Setup Astro for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://smosquera-m.github.io',
+  base: '/portfolio-astro',
+});
